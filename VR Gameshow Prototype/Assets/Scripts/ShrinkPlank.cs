0d33f6fe8fc3b997f81cc29cc058bedcc64eb7e7
PamWA <pambalam6782@gmail.com>
2021-08-29 19:06:22 +0800

    Added in buttons in main scene
    Added in button for shortening and lengthening the plank plus a play button (doesnt do much atm)

--- VR Gameshow Prototype/Assets/Scripts/ShrinkPlank.cs
+++ VR Gameshow Prototype/Assets/Scripts/ShrinkPlank.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class ShrinkPlank : MonoBehaviour
 {
-    [SerializeField] float plankLength;
-    /*[HideInInspector]*/ public float currentPlankLength;
+    //[SerializeField] private float plankLength;
+    //[HideInInspector] public float currentPlankLength;
 
-    public void StartShrinking(float amountToShrink)
+	[SerializeField] private float speed;
+	private bool shrinking;
+	private float targetLocation;
+
+	public void StartShrinking(float amountToShrink)
     {
-        while (transform.position.x > (transform.position.x - amountToShrink))
-        {
-            Debug.Log("it is still greater than where it needs to be");
-        }
-    }
+		targetLocation = transform.position.x - amountToShrink; // Maybe if you added "- targetLocation" after amountToShrink it would also allow this to be acted on twice in the same shrinking time without screwing up the final location
+		shrinking = true;
+	}
+
+	private void Update()
+	{
+		if (shrinking == true)
+		{
+			var vectorTarget = new Vector3(targetLocation, transform.position.y, transform.position.z);
+			transform.position = Vector3.MoveTowards(transform.position, vectorTarget, Time.deltaTime * speed);
+			if (transform.position.x == targetLocation)
+			{
+				shrinking = false;
+			}
+		}
+	}
 }
